ad2549e931de80ca0515d13a4226c1caea350d80
aliaksei-andreyeu <aliaksei.andreyeu@gmail.com>
2023-11-16 19:12:31 +0300

    Submit final task for Module 4.1

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,6 +2,32 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
+        if (year>=0 && (month >=1 && month <=12)) {
+            if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
+                System.out.println("31");
+            } else {
+                if (month == 2) {
+                    if (ifYearLeap(year)){
+                        System.out.println("29");
+                    } else {
+                        System.out.println("28");
+                    }
+                } else {
+                    System.out.println("30");
+                }
+            }
+        } else {
+            System.out.println("invalid date");
+        }
+    }
 
+    public boolean ifYearLeap(int year) {
+        boolean ifYearLeap = year % 4 == 0;
+        if (year%100==0) {
+            if (year % 4 != 0 || year % 400 != 0) {
+                ifYearLeap = false;
+            }
+        }
+        return ifYearLeap;
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
@@ -2,6 +2,17 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class TriangleSidesValidator {
     public void validate(double firstSide, double secondSide, double thirdSide) {
-
+        double firstCouple = firstSide + secondSide;
+        double secondCouple = thirdSide + secondSide;
+        double thirdCouple = firstSide + thirdSide;
+        if (firstSide > 0 && secondSide > 0 && thirdSide > 0) {
+            if (firstCouple > thirdSide && secondCouple > firstSide && thirdCouple > secondSide) {
+                System.out.println("this is a valid triangle");
+            } else {
+                System.out.println("it's not a triangle");
+            }
+        } else {
+            System.out.println("it's not a triangle");
+        }
     }
 }
